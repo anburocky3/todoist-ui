0ae8e1df0be844955b4c1678d2a9670c92359854
Anbuselvan Rocky <anbuceo@gmail.com>
2019-05-25 21:35:42 +0530

    Revert "installing  i18n files"
    This reverts commit 97fa62ae80ee42e30b3cd0f7385179efeefc3489.

--- src/main.js
+++ src/main.js
@@ -3,25 +3,19 @@ import Master from "./components/layouts/Master";
 import router from "./routers";
 import store from "./store";
 import VeeValidate from 'vee-validate';
-
 // import interceptorsSetup from './helpers/interceptors'
-// import axios from 'axios'
-import VueI18n from 'vue-i18n'
-
-import i18n from './i18n'
-
+import axios from 'axios'
 
 Vue.use(VeeValidate);
-Vue.use(VueI18n)
 
 // interceptorsSetup()
 // console.log(router);
 
 
-// const token = localStorage.getItem('access_token')
-// if (token) {
-//   axios.defaults.headers.common['Authorization'] = token
-// }
+const token = localStorage.getItem('access_token')
+if (token) {
+  axios.defaults.headers.common['Authorization'] = token
+}
 
 Vue.prototype.$appName = 'Todoist'
 
@@ -30,6 +24,5 @@ Vue.config.productionTip = false;
 new Vue({
   store,
   router,
-  i18n,
   render: h => h(Master)
 }).$mount("#app");
